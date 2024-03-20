90de2da4d13e9ef8096d3f7788a9872e8faa4c44
mayapakhe123 <157562506+mayapakhe123@users.noreply.github.com>
2024-03-20 09:33:55 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Technogeeks Technologies";
+        return "DevOps & Jenkins Project for Technogeeks Technologies";
     }
 }
